14c58f5153eba58eed46c8b45189a27d28f5ed08
Marcos Ruiz Freire <marcos.ruizfreire-ext@ab-inbev.com>
2026-08-11 19:53:08 -0300

    fix: Add persist annotation to date lastChange field

--- src/main/java/com/restaurant/management/model/User.java
+++ src/main/java/com/restaurant/management/model/User.java
@@ -10,6 +10,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.Getter;
@@ -50,5 +52,11 @@ public abstract class User {
 
     @Embedded
     private Address address;
+
+    @PrePersist
+    @PreUpdate
+    private void updateLastChange() {
+        lastChange = LocalDateTime.now();
+    }
 
 }
